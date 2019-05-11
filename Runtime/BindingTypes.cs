aeaaf41bfd9119993686e4f9a2de0066ae1c8651
johnsoncodehk <johnsoncodehk@gmail.com>
2019-05-11 23:32:13 +0800

    Rename SubTypes

--- Runtime/BindingTypes.cs
+++ Runtime/BindingTypes.cs
@@ -5,96 +5,96 @@ using UnityEngine.Events;
 namespace uBinding
 {
 
-    [Serializable] public class StringEventHandler : UnityEvent<string> { }
-    [Serializable] public class IntEventHandler : UnityEvent<int> { }
-    [Serializable] public class FloatEventHandler : UnityEvent<float> { }
-    [Serializable] public class BoolEventHandler : UnityEvent<bool> { }
+    [Serializable] public class StringBindingEvent : UnityEvent<string> { }
+    [Serializable] public class IntBindingEvent : UnityEvent<int> { }
+    [Serializable] public class FloatBindingEvent : UnityEvent<float> { }
+    [Serializable] public class BoolBindingEvent : UnityEvent<bool> { }
 
-    [Serializable] public class ColorEventHandler : UnityEvent<Color> { }
-    [Serializable] public class GradientEventHandler : UnityEvent<Gradient> { }
-    [Serializable] public class Matrix4x4EventHandler : UnityEvent<Matrix4x4> { }
-    [Serializable] public class QuaternionEventHandler : UnityEvent<Quaternion> { }
-    [Serializable] public class Vector2EventHandler : UnityEvent<Vector2> { }
-    [Serializable] public class Vector3EventHandler : UnityEvent<Vector3> { }
-    [Serializable] public class Vector4EventHandler : UnityEvent<Vector4> { }
-    [Serializable] public class Vector2IntEventHandler : UnityEvent<Vector2Int> { }
-    [Serializable] public class Vector3IntEventHandler : UnityEvent<Vector3Int> { }
+    [Serializable] public class ColorBindingEvent : UnityEvent<Color> { }
+    [Serializable] public class GradientBindingEvent : UnityEvent<Gradient> { }
+    [Serializable] public class Matrix4x4BindingEvent : UnityEvent<Matrix4x4> { }
+    [Serializable] public class QuaternionBindingEvent : UnityEvent<Quaternion> { }
+    [Serializable] public class Vector2BindingEvent : UnityEvent<Vector2> { }
+    [Serializable] public class Vector3BindingEvent : UnityEvent<Vector3> { }
+    [Serializable] public class Vector4BindingEvent : UnityEvent<Vector4> { }
+    [Serializable] public class Vector2IntBindingEvent : UnityEvent<Vector2Int> { }
+    [Serializable] public class Vector3IntBindingEvent : UnityEvent<Vector3Int> { }
 
     [Serializable]
-    public class BindableString : Binding<string, StringEventHandler, BindableString>
+    public class StringBinding : Binding<string, StringBindingEvent, StringBinding>
     {
-        public BindableString(string value) : base(value) { }
+        public StringBinding(string value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableInt : Binding<int, IntEventHandler, BindableInt>
+    public class IntBinding : Binding<int, IntBindingEvent, IntBinding>
     {
-        public BindableInt(int value) : base(value) { }
+        public IntBinding(int value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableFloat : Binding<float, FloatEventHandler, BindableFloat>
+    public class FloatBinding : Binding<float, FloatBindingEvent, FloatBinding>
     {
-        public BindableFloat(float value) : base(value) { }
+        public FloatBinding(float value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableBool : Binding<bool, BoolEventHandler, BindableBool>
+    public class BoolBinding : Binding<bool, BoolBindingEvent, BoolBinding>
     {
-        public BindableBool(bool value) : base(value) { }
+        public BoolBinding(bool value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableColor : Binding<Color, ColorEventHandler, BindableColor>
+    public class ColorBinding : Binding<Color, ColorBindingEvent, ColorBinding>
     {
-        public BindableColor(Color value) : base(value) { }
+        public ColorBinding(Color value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableGradient : Binding<Gradient, GradientEventHandler, BindableGradient>
+    public class GradientBinding : Binding<Gradient, GradientBindingEvent, GradientBinding>
     {
-        public BindableGradient(Gradient value) : base(value) { }
+        public GradientBinding(Gradient value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableMatrix4x4 : Binding<Matrix4x4, Matrix4x4EventHandler, BindableMatrix4x4>
+    public class Matrix4x4Binding : Binding<Matrix4x4, Matrix4x4BindingEvent, Matrix4x4Binding>
     {
-        public BindableMatrix4x4(Matrix4x4 value) : base(value) { }
+        public Matrix4x4Binding(Matrix4x4 value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableQuaternion : Binding<Quaternion, QuaternionEventHandler, BindableQuaternion>
+    public class QuaternionBinding : Binding<Quaternion, QuaternionBindingEvent, QuaternionBinding>
     {
-        public BindableQuaternion(Quaternion value) : base(value) { }
+        public QuaternionBinding(Quaternion value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableVector2 : Binding<Vector2, Vector2EventHandler, BindableVector2>
+    public class Vector2Binding : Binding<Vector2, Vector2BindingEvent, Vector2Binding>
     {
-        public BindableVector2(Vector2 value) : base(value) { }
+        public Vector2Binding(Vector2 value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableVector3 : Binding<Vector3, Vector3EventHandler, BindableVector3>
+    public class Vector3Binding : Binding<Vector3, Vector3BindingEvent, Vector3Binding>
     {
-        public BindableVector3(Vector3 value) : base(value) { }
+        public Vector3Binding(Vector3 value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableVector4 : Binding<Vector4, Vector4EventHandler, BindableVector4>
+    public class Vector4Binding : Binding<Vector4, Vector4BindingEvent, Vector4Binding>
     {
-        public BindableVector4(Vector4 value) : base(value) { }
+        public Vector4Binding(Vector4 value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableVector2Int : Binding<Vector2Int, Vector2IntEventHandler, BindableVector2Int>
+    public class Vector2IntBinding : Binding<Vector2Int, Vector2IntBindingEvent, Vector2IntBinding>
     {
-        public BindableVector2Int(Vector2Int value) : base(value) { }
+        public Vector2IntBinding(Vector2Int value) : base(value) { }
     }
 
     [Serializable]
-    public class BindableVector3Int : Binding<Vector3Int, Vector3IntEventHandler, BindableVector3Int>
+    public class Vector3IntBinding : Binding<Vector3Int, Vector3IntBindingEvent, Vector3IntBinding>
     {
-        public BindableVector3Int(Vector3Int value) : base(value) { }
+        public Vector3IntBinding(Vector3Int value) : base(value) { }
     }
 }
